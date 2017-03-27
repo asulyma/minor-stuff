6582f01f0642484be2ae2aaefbe682a7bb2f17f7
Aleksandr <localprogrammer04@gmail.com>
2017-03-27 15:39:37 +0300

    Example deadlock complete

--- src/other/Deadlock.java
+++ src/other/Deadlock.java
@@ -1,5 +1,27 @@
 package other;
 
-public class Deadlock {
+public class Deadlock implements Runnable {
 
+    SynchronizedClassA a = new SynchronizedClassA();
+    SynchronizedClassB b = new SynchronizedClassB();
+
+    public Deadlock() {
+        Thread.currentThread().setName("MainThread");
+        Thread t = new Thread(this, "RacingThread");
+        t.start();
+
+        a.foo(b);
+
+        System.out.println("Назад в главный поток");
+    }
+
+    @Override
+    public void run() {
+        b.bar(a);
+        System.out.println("Назад в другой поток");
+    }
+
+    public static void main(String[] args) {
+        new Deadlock();
+    }
 }
