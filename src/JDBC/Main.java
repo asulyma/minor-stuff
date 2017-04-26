61b72e0783cecf84f402e761e7f9b8aebef8a3f7
Aleksandr <localprogrammer04@gmail.com>
2017-04-26 12:54:47 +0300

    Statement "JDBC"

--- src/JDBC/Main.java
+++ src/JDBC/Main.java
@@ -1,25 +1,27 @@
 package JDBC;
 
-import java.sql.Connection;
-import java.sql.Driver;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Main {
     private static final String USERNAME = "root";
     private static final String PASSWORD = "root";
-    private static final String URL = "jdbc:mysql://localhost:3306/mysql";
+    private static final String URL = "jdbc:mysql://localhost:3306/mysql?useSSL=false";
 
     public static void main(String[] args) throws SQLException {
         Connection connection = null;
         Driver driver;
+        Statement statement;
 
         try {
             driver = new com.mysql.jdbc.Driver();       //download driver
             DriverManager.registerDriver(driver);       //registered driver
             connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            statement = connection.createStatement();   //create statement for commands
 
-            System.out.println("Realization...");
+            //statement.execute("INSERT INTO business.products (product_name, price, shop_id) VALUES (\"Sosages\", 44,2)");
+            //statement.executeUpdate("UPDATE business.products SET product_name=\"Snickers\" WHERE product_id=7");
+            //ResultSet resultSet = statement.executeQuery("SELECT * FROM business.products");
+
 
         } catch (SQLException ex) {
             System.out.println("Error!");
