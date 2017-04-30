0bcd242a759534ad7b8a7bb1449c8f8f0e7191c4
Aleksandr <localprogrammer04@gmail.com>
2017-04-30 11:33:54 +0300

    PreparedStatement "JDBC"

--- src/JDBC/Main.java
+++ src/JDBC/Main.java
@@ -12,22 +12,32 @@ public class Main {
         String tmpName;
         double tmpPrice;
 
-        Connection connection = new DBProcessor().getConnection(URL,USERNAME,PASSWORD);
-        String query = "select * from business.products";
+        Connection connection = new DBProcessor().getConnection(URL, USERNAME, PASSWORD);
         Statement statement = connection.createStatement();
 
-        ResultSet resultSet = statement.executeQuery(query);
+        String query = "select * from business.products";
+        PreparedStatement preparedQuery = connection.prepareStatement(query);
 
-        while (resultSet.next()){
+        String insert = "insert into business.products (product_name, price, shop_id) values (?, ?, ?)";
+        PreparedStatement preparedInsert = connection.prepareStatement(insert);
+
+        preparedInsert.setString(1, "PreparedFood");
+        preparedInsert.setDouble(2, 98.9);
+        preparedInsert.setInt(3, 2);
+        preparedInsert.execute();
+
+
+        ResultSet resultSet = preparedQuery.executeQuery(query);
+        while (resultSet.next()) {
             tmpID = resultSet.getInt("product_id");
             tmpName = resultSet.getString("product_name");
             tmpShopID = resultSet.getInt("shop_id");
             tmpPrice = resultSet.getDouble("price");
-            Product product = new Product(tmpID, tmpName,tmpPrice,tmpShopID);
+            Product product = new Product(tmpID, tmpName, tmpPrice, tmpShopID);
             System.out.println(product);
         }
 
-        statement.close();
+        preparedQuery.close();
         connection.close();
     }
 }
