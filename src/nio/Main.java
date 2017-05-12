6d1393c22d05a0f979def35b8eb7129d4b43aa6c
Aleksandr <localprogrammer04@gmail.com>
2017-05-12 11:12:38 +0300

    NIO testing(update)

--- src/nio/Main.java
+++ src/nio/Main.java
@@ -1,40 +1,20 @@
 package nio;
 
 import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.channels.SeekableByteChannel;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
 import java.nio.file.Files;
-import java.nio.file.InvalidPathException;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Main {
     public static void main(String[] args) {
-        int count;
-        Path filePath = null;
+        //Get channel to file through the path
+        try (FileChannel fChannel = (FileChannel) Files.newByteChannel(Paths.get("D:\\test.txt"))) {
+            long fileSize = fChannel.size();
+            MappedByteBuffer mappedByteBuffer = fChannel.map(FileChannel.MapMode.READ_ONLY, 0, fileSize);
 
-        //Path to file
-        try {
-            filePath = Paths.get("D:\\test.txt");
-        } catch (InvalidPathException e) {
-            System.out.println("Path error " + e);
-            return;
-        }
-
-        //Get channel to file
-        try (SeekableByteChannel fChannel = Files.newByteChannel(filePath)) {
-            ByteBuffer byteBuffer = ByteBuffer.allocate(128);
-            do {
-                count = fChannel.read(byteBuffer);
-                //if !end of file
-                if (count != -1) {
-                    byteBuffer.rewind();
-                    for (int i = 0; i < count; i++) {
-                        //read and display symbols
-                        System.out.println((char) byteBuffer.get());
-                    }
-                }
-            } while (count != -1);
+            for (int i = 0; i < fileSize; i++)
+                System.out.println((char) mappedByteBuffer.get());
             System.out.println();
         } catch (IOException e) {
             System.out.println("IO Error " + e);
