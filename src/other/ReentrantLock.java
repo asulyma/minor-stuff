87c7a90bd84933f02d142240e6ff786ed438ca86
Aleksandr <localprogrammer04@gmail.com>
2017-06-18 23:41:54 +0300

    example tryLock

--- src/other/ReentrantLock.java
+++ src/other/ReentrantLock.java
@@ -3,43 +3,38 @@ package other;
 import java.util.concurrent.locks.Lock;
 
 public class ReentrantLock {
+    static Lock lock = new java.util.concurrent.locks.ReentrantLock();
+
     public static void main(String[] args) throws InterruptedException {
-        Resource resource = new Resource();
-        resource.i = 5;
-        MyThread myThreadFirst = new MyThread();
-        myThreadFirst.setName("one");
-        MyThread myThreadSecond = new MyThread();
-        myThreadFirst.resource = resource;
-        myThreadSecond.resource = resource;
-        myThreadFirst.start();
-        myThreadSecond.start();
-        myThreadFirst.join();
-        myThreadSecond.join();
-        System.out.println(resource.i);
+        new MyThread().start();
+        new MyThreadSecond().start();
     }
 
     static class MyThread extends Thread {
-        Resource resource;
-
         @Override
         public void run() {
-            resource.changeI();
+            lock.lock();
+            System.out.println(getName() + " start working");
+            try {
+                sleep(5);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(getName() + " stop working");
+            lock.unlock();
         }
     }
-}
 
-class Resource {
-    int i;
-    private Lock lock = new java.util.concurrent.locks.ReentrantLock();
-
-    void changeI() {
-        lock.lock();
-        int i = this.i;
-        if (Thread.currentThread().getName().equals("one")) {
-            Thread.yield();
+    static class MyThreadSecond extends Thread {
+        @Override
+        public void run() {
+            System.out.println(getName() + " begin working");
+            while (true) {
+                if (lock.tryLock()) {
+                    System.out.println(getName() + " working");
+                    break;
+                } else System.out.println(getName() + " waiting");
+            }
         }
-        i++;
-        this.i = i;
-        lock.unlock();
     }
 }
